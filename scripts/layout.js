5db7d9983d0897657ba3f20278e89984434d0eb9
unknown <Scott@Scott-PC.(none)>
2013-05-12 18:14:29 -0500

    break out style docs. start javascript.  revise layout.

---
+++ scripts/layout.js
@@ -0,0 +1,18 @@
+  $(document).ready(function() {
+
+    // $("#links-life").hide();
+    // $("#header-life").animate({
+    //     opacity: 0.6
+    // });
+
+    // $('#header-skills').click(function() {
+    //     $("#links-life").hide();
+    //     $("#links-skills").show();
+    // });
+    // $('#header-life').click(function() {
+    //     $("#links-skills").hide();
+    //     $("#links-life").show();
+    // });
+
+
+  });
