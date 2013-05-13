4f8246f3f9d2c9f5302a28892fe24dfde16ca210
unknown <Scott@Scott-PC.(none)>
2013-05-12 22:16:02 -0500

    stragglers from last commit from laptop

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,9 +1,30 @@
   $(document).ready(function() {
+    // $('#go-home').click(function() {
+    //     alert("Okay, we'll go home...");
+    //     $("#go-home").stop().animate({ opacity: 0.4 });
+
+    // });
+
+
+    $("#go-home").click(function(){
+         window.location=$(this).find("a").attr("href");
+         return false;
+    });
+
+
+    $("#go-web-dev").click(function(){
+         window.location=$(this).find("a").attr("href");
+         return false;
+    });
+
+    // $("#go-*").click(function(){
+    //      window.location=$(this).find("a").attr("href");
+    //      return false;
+    // });
 
     // $("#links-life").hide();
     // $("#header-life").animate({
     //     opacity: 0.6
-    // });
 
     // $('#header-skills').click(function() {
     //     $("#links-life").hide();
